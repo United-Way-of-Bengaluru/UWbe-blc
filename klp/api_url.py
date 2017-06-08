ffaf961e46a65e1d44f891f95e874ff750cea6a1
Dinesh Kumar Sharma <dinesh829269@gmail.com>
2017-06-08 16:37:38 +0530

    Klp api Url

---
+++ klp/api_url.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from schools.models import *
+from django.conf.urls import url, include
+from rest_framework import routers, serializers, viewsets
+
+
+# Serializers define the API representation.
+class InstitutionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Institution
+        fields = ('boundary', 'dise_code', 'name', 'cat')
+
+# ViewSets define the view behavior.
+class InstitutionViewSet(viewsets.ModelViewSet):
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'institutes', InstitutionViewSet)
+
+
+urlpatterns = [
+     url(r'^', include(router.urls)),
+]
+
--- klp/settings.py
+++ klp/settings.py
@@ -38,10 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'rest_framework',
     'schools',
     #'fullhistory',
     'assessments',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 SITE_ID = 1
 
